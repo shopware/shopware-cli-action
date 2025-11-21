08755e363f185d111022db2928082f3842a612da
Soner Sayakci <s.sayakci@gmail.com>
2025-11-21 14:04:47 +0100

    feat: restructure

--- src/main/context.ts
+++
@@ -1,9 +0,0 @@
-import { arch, platform } from "node:os";
-import { getInput } from "@actions/core";
-
-export const osPlat: string = platform();
-export const osArch: string = arch();
-
-export const inputs = {
-	version: getInput("version") || "latest",
-};
